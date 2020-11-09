d45b42bd9c14a78fe0f6aea1e48973e9c42bdd1d
yangYue <yangyue@qfun.com>
2020-11-09 18:54:40 +0800

    no message

---
+++ Object-CStudent/Lock/Lock/Lock.h
@@ -0,0 +1,18 @@
+//
+//  Lock.h
+//  Lock
+//
+//  Created by mac on 2020/11/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Lock.
+FOUNDATION_EXPORT double LockVersionNumber;
+
+//! Project version string for Lock.
+FOUNDATION_EXPORT const unsigned char LockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lock/PublicHeader.h>
+#import <Lock/LockTest.h>
+
---
+++ Object-CStudent/Lock/LockTests/LockTests.m
@@ -0,0 +1,37 @@
+//
+//  LockTests.m
+//  LockTests
+//
+//  Created by mac on 2020/11/9.
+//
+
+#import <XCTest/XCTest.h>
+#import "LockTest.h"
+@interface LockTests : XCTestCase
+
+@end
+
+@implementation LockTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    [LockTest.new spinLock];
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
