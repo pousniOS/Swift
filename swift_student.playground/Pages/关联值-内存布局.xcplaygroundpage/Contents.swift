d4055e61f1dd83079cd60b8afdc7163c0ad9dbb3
yangYue <yangyue@qfun.com>
2020-11-12 21:17:00 +0800

    no message

---
+++ swift_student.playground/Pages/关联值-内存布局.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: [Previous](@previous)
+
+import Foundation
+//: ## 关联值-内存布局
+//: + 可以使用MemoryLayout获取数据类型占用的内存大小
+var age = 10
+MemoryLayout<Int>.size      //实际用到的大小
+MemoryLayout<Int>.stride    //变量实际占用的空间的大小
+MemoryLayout<Int>.alignment //内存对齐
+
+MemoryLayout.size(ofValue: age)
+MemoryLayout.stride(ofValue: age)
+MemoryLayout.alignment(ofValue: age)
+
+enum Password {
+    case number(Int,Int,Int,Int)
+    case other
+}
+
+MemoryLayout<Password>.size
+MemoryLayout<Password>.alignment
+MemoryLayout<Password>.stride
+
+var pwd = Password.number(1,1,1,1)
+MemoryLayout.size(ofValue: pwd)
+MemoryLayout.stride(ofValue: pwd)
+MemoryLayout.alignment(ofValue: pwd)
+
+enum Season:String{
+    case spring,summer,autumn,winter
+}
+Season.spring.rawValue
+
+MemoryLayout<Season>.size
+MemoryLayout<Season>.alignment
+MemoryLayout<Season>.stride
+//: [Next](@next)
